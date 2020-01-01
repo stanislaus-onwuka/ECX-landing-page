080d5820896f13b6585f893e880f46012baf9b2c
Onwuka Stanley <kachi2505@yahoo.com>
2020-01-01 19:46:47 +0000

    Achievements started

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-ECHO is on.
+const imgSlide = document.querySelector('.slide');
+const images = ['"./images/Achievements slides/Totel_Photography-6587.jpg"','"./images/Achievements slides/Totel_Photography-6634.jpg"','"./images/Achievements slides/Totel_Photography-6772.jpg"','"./images/Achievements slides/Totel_Photography-6915.jpg"'];
+
+
+let x = 0;
+
+function slider() {
+    if (x < images.length){
+        x = x + 1;
+    }else{
+        x = 1;
+    }
+    imgSlide.innerHTML = "<img src="+images[x-1]+" class = slide-img>";
+}
+
+setInterval(slider, 2000);
