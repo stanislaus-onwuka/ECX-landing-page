20fd6fc9ad562b62a723049f36b371d1dcc932a5
Tochukwu Onwunali <onwunali91@yahoo.com>
2020-01-03 17:27:37 +0000

    fixed the nav bug

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 const Mission = document.querySelector(".vision-list1")
 const Vision = document.querySelector(".vision-list2")
 const Philosophy = document.querySelector(".vision-list3")
-
-
 const MissionC = document.querySelector(".content-container1")
 const VisionC = document.querySelector(".content-container2")
 const PhilosophyC = document.querySelector(".content-container3")
@@ -10,7 +8,6 @@ const PhilosophyC = document.querySelector(".content-container3")
 Mission.addEventListener("click",missionFn)
 
 function missionFn() {
-    // console.log("xup")
     MissionC.classList.add("yes-display");
     MissionC.classList.remove("no-display");
     PhilosophyC.classList.add("no-display");
@@ -23,7 +20,6 @@ function missionFn() {
 Vision.addEventListener("click", visionFn)
 
 function visionFn() {
-    // console.log("xup2")
     MissionC.classList.add("no-display");
     PhilosophyC.classList.add("no-display");
     VisionC.classList.add("yes-display");
@@ -36,7 +32,6 @@ function visionFn() {
 Philosophy.addEventListener("click", philoFn)
 
 function philoFn() {
-    // console.log("xup3")
     MissionC.classList.add("no-display");
     PhilosophyC.classList.add("yes-display");
     PhilosophyC.classList.remove("no-display");
@@ -67,14 +62,11 @@ function scrollFn() {
 
 const menu = document.querySelector(".hidden-menu");
 const nav = document.querySelector(".nav");
-const body = document.querySelector("body");
-
 
 const show = function () {
     nav.classList.toggle("sample");
     menu.querySelector(".span2").classList.toggle("spanTrans")
-    body.classList.toggle("overflow-for-body")
+    navC.classList.toggle("nav-stable")
 }
-
 
 menu.addEventListener("click", show)
