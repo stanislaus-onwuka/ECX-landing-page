2ae1f776dcdd54974273c9fc20839b44ee3aba30
Onwuka Stanley <kachi2505@yahoo.com>
2020-01-07 18:31:59 +0000

    Minor fixes

--- js/gallery.js
+++ js/gallery.js
@@ -2,5 +2,10 @@ const galleryContainer = document.querySelector('.gallery');
 const imgArr = [];
 
 galleryContainer.addEventListener('DOMContentLoaded', ()=>{
-    for()
+    for(let i = 0;i<imgArr.length;i++){
+        const newImage = document.createElement('img');
+        newImage.className = ' ';
+        newImage.src = imgArr[i];
+        galleryContainer.appendChild(newImage);
+    }
 })
