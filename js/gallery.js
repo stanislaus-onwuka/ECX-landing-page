80039799f01409131acffaf1a4f3142ff7f97eed
Onwuka Stanley <kachi2505@yahoo.com>
2020-01-08 06:31:15 +0000

    Gallery in progress

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,18 @@
-const galleryContainer = document.querySelector('.gallery');
-const imgArr = [];
+const galleryContainer = document.querySelector('.pictures');
+const imgArr = ['https://res.cloudinary.com/ecx/image/upload/q_auto,f_auto/v1578312244/ECX%20gallery/All%20Pictures/Totel_Photography-7425_jp2bjm.jpg',
+                'https://res.cloudinary.com/ecx/image/upload/q_auto,f_auto/v1578312242/ECX%20gallery/All%20Pictures/Totel_Photography-6856_xfxjmf.jpg',
+                'https://res.cloudinary.com/ecx/image/upload/q_auto,f_auto/v1578312242/ECX%20gallery/All%20Pictures/Totel_Photography-7431_izfscu.jpg'
+                ];
 
-galleryContainer.addEventListener('DOMContentLoaded', ()=>{
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
     for(let i = 0;i<imgArr.length;i++){
         const newImage = document.createElement('img');
-        newImage.className = ' ';
+        const imgDiv = document.createElement('div');
+        imgDiv.className = 'picture';
         newImage.src = imgArr[i];
-        galleryContainer.appendChild(newImage);
+        imgDiv.appendChild(newImage);
+        galleryContainer.appendChild(imgDiv);
     }
 })
