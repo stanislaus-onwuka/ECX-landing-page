c5b919c960740165c3c405bec49ad3e131b6f711
Tochukwu Onwunali <onwunali91@yahoo.com>
2020-01-08 21:23:38 +0000

    Minor adjustments on the gallery

--- js/script.js
+++ js/script.js
@@ -69,17 +69,24 @@ function showSlides(n) {
 }
 
 // Gallery Slider
-var myIndex = 0;
-carousel();
+var slideIndex1 = 1;
+showDivs(slideIndex1);
 
-function carousel() {
+function plusDivs(n) {
+    showDivs(slideIndex1 += n);
+}
+
+function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlides1");
+    if (n > x.length) {
+        slideIndex1 = 1
+    }
+    if (n < 1) {
+        slideIndex1 = x.length
+    }
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    myIndex++;
-    if (myIndex > x.length) { myIndex = 1 }
-    x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 5000); // Change image every 5 seconds
+    x[slideIndex1 - 1].style.display = "block";
 }
