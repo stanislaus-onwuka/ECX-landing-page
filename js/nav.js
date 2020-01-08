25dc6f72236d941a67e691e58c062a383e9a0f61
Onwuka Stanley <kachi2505@yahoo.com>
2020-01-08 13:36:19 +0000

    Nav fix

---
+++ js/nav.js
@@ -0,0 +1,34 @@
+const navC = document.querySelector(".container2");
+var previousScroll = window.scrollY;
+window.addEventListener("scroll", scrollFn);
+
+function scrollFn() {
+    if (window.scrollY < previousScroll) {
+        navC.classList.remove("nav-down")
+        navC.classList.add("nav-up")
+    } else if (window.scrollY > previousScroll) {
+        navC.classList.add("nav-down")
+        navC.classList.remove("nav-up")
+    } else {
+        navC.classList.add("nav-up")
+    }
+    previousScroll = window.scrollY;
+}
+
+const menu = document.querySelector(".hidden-menu");
+const nav = document.querySelector(".nav");
+const menuspan = document.querySelector(".menu-span");
+const menuspan2 = document.querySelector(".menu-span2");
+const menuspan3 = document.querySelector(".menu-span3");
+
+
+const show = function () {
+    nav.classList.toggle("sample");
+    menu.querySelector(".span2").classList.toggle("spanTrans")
+    navC.classList.toggle("nav-stable")
+    menuspan.classList.toggle("menu-span-black")
+    menuspan2.classList.toggle("menu-span-black")
+    menuspan3.classList.toggle("menu-span-black")
+}
+
+menu.addEventListener("click", show)
